40859d209f73385c88626cb9e36882d7bece626c
sunmumu <335089101@qq.com>
2019-01-09 16:12:04 +0800

    添加GDPEmptyDataSet.h文件
    添加GDPEmptyDataSet.h文件, 便于导入名称统一

---
+++ GDPEmptyDataSet/GDPEmptyDataSet.h
@@ -0,0 +1,14 @@
+//
+//  GDPEmptyDataSet.h
+//  51jrqFramework
+//
+//  Created by sunmumu on 2019/1/9.
+//  Copyright © 2019 xiao han. All rights reserved.
+//
+
+#ifndef GDPEmptyDataSet_h
+#define GDPEmptyDataSet_h
+
+#import "UIScrollView+EmptyDataSet.h"
+
+#endif /* GDPEmptyDataSet_h */
